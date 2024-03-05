75226f786145ad2a14f0ef1b4ffd48546924a6bd
okwhy <59335640+okwhy@users.noreply.github.com>
2024-03-05 22:27:06 +0400

    init2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -9,12 +10,15 @@ android {
 
     defaultConfig {
         applicationId = "com.sus.calendar"
-        minSdk = 33
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        kapt {
+            arguments {arg("room.schemaLocation", "$projectDir/schemas")}
+        }
     }
 
     buildTypes {
@@ -41,7 +45,14 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.google.firebase:firebase-inappmessaging:20.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    val room_version = "2.5.2"
+    implementation("androidx.room:room-runtime:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    implementation ("net.sourceforge.jexcelapi:jxl:2.6.12")
+    implementation ("com.applandeo:material-calendar-view:1.9.0-rc03")
 }
--- app/src/main/java/com/sus/calendar/MainActivity.kt
+++ app/src/main/java/com/sus/calendar/MainActivity.kt
@@ -1,11 +1,70 @@
 package com.sus.calendar
 
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    var bottomNavigationView: BottomNavigationView? = null
+    val Calendar_page = R.id.homepage
+    val Export_page = R.id.export
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setRepeatingAlarm()
+        bottomNavigationView = findViewById<View>(R.id.bottomNavigationView) as BottomNavigationView
+        bottomNavigationView!!
+            .setOnNavigationItemSelectedListener(this)
+        bottomNavigationView!!.selectedItemId = R.id.homepage
+    }
+
+    var calendarFragment = CalendarFragment()
+    var exportFragment = ExportFragment()
+    private fun setRepeatingAlarm() {
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+
+        // Создаем Intent для нашего BroadcastReceiver
+        val intent = Intent(this, AlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        // Устанавливаем интервал в 24 часа
+        val repeatInterval = (60 * 60 * 1000).toLong() // час в миллисекундах
+
+        // Устанавливаем повторяющееся событие
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            System.currentTimeMillis() + repeatInterval,
+            repeatInterval,
+            pendingIntent
+        )
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == Calendar_page) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.flFragment, calendarFragment)
+                .commit()
+            true
+        } else if (item.itemId == Export_page) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.flFragment, exportFragment)
+                .commit()
+            true
+        } else false
     }
 }
